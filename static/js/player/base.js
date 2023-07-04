fdbd83097c1b5af954951ee87a4b14c1b1c2dc40
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-07-04 15:58:53 -0700

    Add: player movement

--- static/js/player/base.js
+++ static/js/player/base.js
@@ -12,6 +12,15 @@ export class Player extends AcGameObject{
         this.height = info.height;
         this.color = info.color;
 
+        this.vx = 0;
+        this.vy = 0;
+
+        this.speedx = 400; // horizontal moving speed
+        this.speedy = 1000; // vertical moving speed
+        
+        this.direction = 1;
+        this.gravity = 50;
+
         this.ctx = this.root.game_map.ctx;
     }
 
@@ -19,7 +28,20 @@ export class Player extends AcGameObject{
 
     }
 
+    move(){
+        this.vy += this.gravity;
+
+        this.x += this.vx * this.timeDelta / 1000;
+        this.y += this.vy * this.timeDelta / 1000;
+
+        if (this.y > 450){
+            this.y = 450;
+            this.vy = 0;
+        }
+    }
+
     update(){
+        this.move();
         this.render();
     }
 
